7fa9bf44c13e0a023dfa1fac85641f785cf0ce9b
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-09-06 12:46:04 +0100

    fix bug

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -29,33 +29,14 @@ describe('Thermostat', function() {
 
     describe('when power saving is off', function() {
       it('increases current temperature by 1', function() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        this.switchPowerSaving();
-=======
         thermostat.switchPowerSaving()
->>>>>>> 7943b948c35355a7276ee04a19d504b6ec9e3f63
-=======
-        thermostat.switchPowerSaving()
->>>>>>> 7943b948c35355a7276ee04a19d504b6ec9e3f63
         thermostat.increaseTemperature();
         expect(thermostat.currentTemp()).toEqual(21);
       });
 
       it('throws an error if temp is at max', function(){
-<<<<<<< HEAD
-<<<<<<< HEAD
-        this.switchPowerSaving();
-        console.log(this.powerSaving)
-        thermostat.temperature = this.SAVING_OFF_MAX_TEMP;
-=======
         thermostat.switchPowerSaving()
         thermostat.temperature = thermostat.SAVING_OFF_MAX_TEMP;
->>>>>>> 7943b948c35355a7276ee04a19d504b6ec9e3f63
-=======
-        thermostat.switchPowerSaving()
-        thermostat.temperature = thermostat.SAVING_OFF_MAX_TEMP;
->>>>>>> 7943b948c35355a7276ee04a19d504b6ec9e3f63
         expect(function() {thermostat.increaseTemperature()}).toThrow("Temperature at max: can't increase temperature")
       });
     });
---
+++ src/Thermostat.js
@@ -0,0 +1,53 @@
+function Thermostat() {
+  DEFAULT_TEMPERATURE = 20;
+  this.SAVING_ON_MAX_TEMP = 25;
+  this.SAVING_OFF_MAX_TEMP = 32;
+  this.temperature = DEFAULT_TEMPERATURE;
+  this.MINIMUM = 10;
+  this.powerSaving = true;
+}
+
+Thermostat.prototype.currentTemp = function() {
+  return this.temperature;
+};
+
+Thermostat.prototype.increaseTemperature = function() {
+
+  if (this.powerSaving === true)
+    if (this.temperature < this.SAVING_ON_MAX_TEMP) {
+      this.temperature ++;
+    }
+    else {
+      throw "Temperature at max: switch off power saving";
+    }
+  else {
+    // power saving is always true
+    if (this.temperature < this.SAVING_OFF_MAX_TEMP) {
+      this.temperature ++;
+    }
+    else {
+      throw "Temperature at max: can't increase temperature";
+    }
+  }
+};
+
+Thermostat.prototype.decreaseTemperature = function() {
+  if (this.temperature > this.MINIMUM) {
+    this.temperature --;
+  }
+  else {
+    throw 'NOT GONNA WORK BITCH';
+  }
+};
+
+Thermostat.prototype.switchPowerSaving = function() {
+  (this.powerSaving ? this.powerSaving = false : this.powerSaving = true)
+};
+
+// Thermostat.prototype.maxTemp = function() {
+//   (this.powerSaving ? return this.SAVING_ON_MAX_TEMP : return this.SAVING_OFF_MAX_TEMP)
+// };
+//
+// Thermostat.prototype.maxError = function() {
+//   (this.powerSaving ? throw "Temperature at max: switch off power saving" : throw "Temperature at max: can't increase temperature")
+// };
